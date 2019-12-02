e0fd261a091d783a9d7f5daf49c2e657298d02a6
Igor Zubenko <orwir.null@gmail.com>
2019-12-02 17:39:02 +0100

    started implementation of video content rendering.

--- app/src/main/java/orwir/gazzit/videoplayer/VideoPlayerScreen.kt
+++ app/src/main/java/orwir/gazzit/videoplayer/VideoPlayerScreen.kt
@@ -1,34 +1,60 @@
 package orwir.gazzit.videoplayer
 
+import android.content.Context
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
+import com.google.android.exoplayer2.ExoPlayer
 import org.koin.android.ext.android.inject
-import orwir.gazzit.databinding.FragmentVideoplayerBinding
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import orwir.gazzit.R
+import orwir.gazzit.common.launchActivity
+import orwir.gazzit.databinding.ActivityVideoplayerBinding
 
-class VideoPlayerFragment : Fragment() {
+private const val EXTRA_URI = "extra_uri"
+
+class VideoPlayerActivity : AppCompatActivity() {
+
+    companion object {
+        fun play(context: Context, uri: Uri) =
+            context.launchActivity(VideoPlayerActivity::class.java) {
+                putExtra(EXTRA_URI, uri)
+            }
+    }
 
     private val viewModel: VideoPlayerViewModel by inject()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = FragmentVideoplayerBinding.inflate(inflater, container, false)
-        .also {
-            it.viewModel = viewModel
-            it.lifecycleOwner = viewLifecycleOwner
-        }
-        .root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil
+            .setContentView<ActivityVideoplayerBinding>(this, R.layout.activity_videoplayer)
+            .also {
+                it.viewModel = viewModel
+                it.lifecycleOwner = this
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viewModel.player.playWhenReady = false
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
 }
 
-class VideoPlayerViewModel : ViewModel() {
+class VideoPlayerViewModel : ViewModel(), KoinComponent {
+
+    val player: ExoPlayer by inject()
 
     override fun onCleared() {
         super.onCleared()
+        player.release()
     }
 
 }
