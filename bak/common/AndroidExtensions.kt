aef85e0b1c717e92b4df4227b51daed1bb39b809
Igor Zubenko <orwir.null@gmail.com>
2019-12-17 17:08:49 +0100

    #13: removed bak files

--- bak/common/AndroidExtensions.kt
+++
@@ -1,9 +0,0 @@
-package orwir.gazzit.common
-
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-
-fun Context.launchActivity(clazz: Class<out AppCompatActivity>, builder: Intent.() -> Unit = {}) {
-    startActivity(Intent(this, clazz).also(builder))
-}
--- bak/common/Authorization.kt
+++
@@ -1,9 +0,0 @@
-package orwir.gazzit.common
-
-import okhttp3.Interceptor
-
-interface AuthorizationHolder {
-    fun request()
-}
-
-interface AuthorizationInterceptor : Interceptor
