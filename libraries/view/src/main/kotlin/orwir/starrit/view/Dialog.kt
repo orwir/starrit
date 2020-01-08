d62df528db84dbc710a103dd280449d00a9314f1
Igor Zubenko <orwir.null@gmail.com>
2020-01-08 02:33:15 +0100

    small improvements

--- libraries/view/src/main/kotlin/orwir/starrit/view/Dialog.kt
+++ libraries/view/src/main/kotlin/orwir/starrit/view/Dialog.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-fun Fragment.showErrorDialog(
-    error: Exception,
-    builder: MaterialAlertDialogBuilder.() -> Unit = {}
-): AlertDialog {
+fun Fragment.showErrorDialog(error: Exception, builder: MaterialAlertDialogBuilder.() -> Unit = {}): AlertDialog {
     val message =
         if (BuildConfig.DEBUG) error.toString() else getString(R.string.error_message_internal)
     return showErrorDialog(message, builder)
