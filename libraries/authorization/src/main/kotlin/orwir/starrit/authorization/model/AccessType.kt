9a23953e040bc140f891daf8bc78ae08b5e1f0e1
Igor Zubenko <orwir.null@gmail.com>
2020-01-10 16:24:37 +0100

    #55: added one more access type; added docs

--- libraries/authorization/src/main/kotlin/orwir/starrit/authorization/model/AccessType.kt
+++ libraries/authorization/src/main/kotlin/orwir/starrit/authorization/model/AccessType.kt
@@ -4,9 +4,25 @@ import orwir.starrit.core.BuildConfig.REDDIT_URL_BASIC
 import orwir.starrit.core.BuildConfig.REDDIT_URL_OAUTH
 
 enum class AccessType {
+    /**
+     * User opens the app first time or cleared data
+     */
     Unspecified,
+    /**
+     * User had authorization but token refresh was unsuccessful
+     */
+    Revoked,
+    /**
+     * User has authorization
+     */
     Authorized,
+    /**
+     * User has access without authorization
+     */
     Anonymous;
 
+    /**
+     * If user [Authorized] send requests to [REDDIT_URL_OAUTH] otherwise [REDDIT_URL_BASIC].
+     */
     fun resolveBaseUrl() = if (this == Authorized) REDDIT_URL_OAUTH else REDDIT_URL_BASIC
 }
