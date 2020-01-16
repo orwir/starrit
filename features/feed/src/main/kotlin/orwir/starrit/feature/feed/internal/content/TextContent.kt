5b23966ed30d158e9746365c380f61eed3eadca1
Igor Zubenko <orwir.null@gmail.com>
2020-01-16 23:32:46 +0100

    #64: improved text layout

--- features/feed/src/main/kotlin/orwir/starrit/feature/feed/internal/content/TextContent.kt
+++ features/feed/src/main/kotlin/orwir/starrit/feature/feed/internal/content/TextContent.kt
@@ -2,14 +2,41 @@ package orwir.starrit.feature.feed.internal.content
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import orwir.starrit.core.event.LiveEvent
 import orwir.starrit.feature.feed.databinding.ViewContentTextBinding
 import orwir.starrit.listing.feed.TextPost
-
-// todo: improve text content layout
 
 @Suppress("FunctionName")
 internal fun PostContentBinder.TextContent(post: TextPost, parent: ViewGroup): View =
     ViewContentTextBinding
         .inflate(inflater, parent, true)
-        .also { it.post = post }
+        .apply {
+            val vm = TextViewModel(post)
+            viewModel = vm
+            lifecycleOwner = owner
+
+            text.post { vm.validateCollapsed(text.lineCount) }
+            vm.expandEvent.observe(owner, Observer { text.maxLines = Int.MAX_VALUE })
+        }
         .root
+
+internal class TextViewModel(post: TextPost) {
+
+    val content = post.text
+    val maxLines = 8
+    val collapsed: LiveData<Boolean> = MutableLiveData()
+    val expandEvent = LiveEvent<Unit>()
+
+    fun validateCollapsed(lines: Int) {
+        (collapsed as MutableLiveData).value = lines > maxLines
+    }
+
+    fun expand() {
+        (collapsed as MutableLiveData).value = false
+        expandEvent.call()
+    }
+
+}
