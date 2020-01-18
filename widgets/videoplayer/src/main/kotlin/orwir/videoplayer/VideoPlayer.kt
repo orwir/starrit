39292b794e675d56a1b26df53270e35139bad7e8
Igor Zubenko <orwir.null@gmail.com>
2020-01-18 23:13:07 +0100

    #76: updated UI; updated behavior.

--- widgets/videoplayer/src/main/kotlin/orwir/videoplayer/VideoPlayer.kt
+++ widgets/videoplayer/src/main/kotlin/orwir/videoplayer/VideoPlayer.kt
@@ -1,7 +1,6 @@
 package orwir.videoplayer
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -12,37 +11,54 @@ import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.source.MediaSource
 import kotlinx.android.synthetic.main.videoplayer.view.*
-import kotlinx.android.synthetic.main.videoplayer_controls.view.*
 
 class VideoPlayer @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr), Player.EventListener {
 
-    lateinit var player: ExoPlayer
     val cover: ImageView by lazy { vp_cover }
-
+    lateinit var player: ExoPlayer
     private lateinit var video: MediaSource
-    private val listener = object : Player.EventListener {
-        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-            when (playbackState) {
-                Player.STATE_BUFFERING -> {
-                }
-                Player.STATE_READY -> {
-                }
-                Player.STATE_IDLE -> {
-                }
-                Player.STATE_ENDED -> {
-                }
-            }
-        }
-    }
+    private var initial = true
+    private var started = false
 
     init {
         LayoutInflater.from(context).inflate(R.layout.videoplayer, this, true)
-        veil.setOnClickListener { start() }
-        exo_stop.setOnClickListener { stop() }
+        beforeStart()
+        vp_veil.setOnClickListener {
+            if (initial && !started) {
+                start()
+            } else {
+                showHUD(show = vp_fullscreen.visibility != View.VISIBLE, state = started && player.playWhenReady)
+            }
+        }
+        vp_play.setOnClickListener { if (!started) start() else playPause() }
+        vp_pause.setOnClickListener { playPause() }
+        vp_repeat.setOnClickListener { }
+        vp_volume.setOnClickListener { }
+        // todo: content seeker
+        vp_open_volume.setOnClickListener { }
+        // todo: volume seeker
+        vp_fullscreen.setOnClickListener { }
+    }
+
+    fun setVideo(uri: Uri) {
+        video = uri.toMediaSource(context)
+    }
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        if (playbackState == Player.STATE_ENDED) {
+            showHUD(show = true, state = false)
+            started = false
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        initial = true
+        started = false
     }
 
     override fun onDetachedFromWindow() {
@@ -50,33 +66,54 @@ class VideoPlayer @JvmOverloads constructor(
         VideoPlayerHolder.releaseSelf(this)
     }
 
-    fun setVideo(uri: Uri) {
-        video = uri.toMediaSource(context)
-    }
-
-    fun setCover(drawable: Drawable?) {
-        vp_cover.setImageDrawable(drawable)
-    }
-
-    fun start() {
-        VideoPlayerHolder.swap(this)
-        vp_content.player = player
-        player.addListener(listener)
-        player.prepare(video, true, true)
-        player.audioComponent?.volume = 0F
-        player.playWhenReady = true
-        vp_content.visibility = View.VISIBLE
-        vp_content.hideController()
-    }
-
-    fun stop() {
-        player.removeListener(listener)
-        player.stop(true)
-        vp_content.visibility = View.GONE
-    }
-
     internal fun release() {
+        player.removeListener(this)
         stop()
+    }
+
+    private fun beforeStart() {
+        vp_surface.visible(false)
+        showHUD(show = true, state = null)
+    }
+
+    private fun start() {
+        VideoPlayerHolder.swap(this)
+        vp_surface.player = player
+        vp_surface.visible(true)
+        showHUD(show = false, state = true)
+        player.prepare(video, true, true)
+        player.audioComponent?.volume = 0F // todo: get from configuration
+        player.playWhenReady = true
+        player.addListener(this)
+        initial = false
+        started = true
+    }
+
+    private fun playPause() {
+        player.playWhenReady = !player.playWhenReady
+        showHUD(show = !player.playWhenReady, state = false)
+    }
+
+    private fun stop() {
+        player.stop(true)
+        started = false
+        beforeStart()
+    }
+
+    private fun showHUD(show: Boolean, state: Boolean?) {
+        vp_play.visible(show && state != true)
+        vp_pause.visible(show && state == true)
+        vp_repeat.visible(show && state != true)
+        vp_volume.visible(show && state == null)
+        //todo: content seeker -> state != null
+        vp_remained.visible(show && state != null)
+        vp_open_volume.visible(show && state != null)
+        //todo: volume seeker -> false
+        vp_fullscreen.visible(show)
+    }
+
+    private fun View.visible(value: Boolean) {
+        visibility = if (value) View.VISIBLE else View.GONE
     }
 
 }
