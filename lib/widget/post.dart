67f22a778a29852ea30602ffbdd52076a1dd9f0b
orwir <orwir.null@gmail.com>
2020-02-24 15:08:38 +0100

    post improvements: * refactored post view structure * added share action

--- lib/widget/post.dart
+++ lib/widget/post.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:share/share.dart';
 import 'package:starrit/model/post.dart';
 import 'package:starrit/util/content.dart';
 import 'package:starrit/util/date.dart';
@@ -9,14 +10,11 @@ import 'package:starrit/widget/content/link.dart';
 import 'package:starrit/widget/content/text.dart';
 import 'package:starrit/widget/content/video.dart';
 
-enum Header { simple, extended }
-
 class PostView extends StatelessWidget {
   final Post post;
-  final Header header;
-  final NumberFormat numberFormat = NumberFormat.compact();
+  final HeaderType header;
 
-  PostView(this.post, {this.header = Header.extended});
+  PostView(this.post, {this.header = HeaderType.extended});
 
   @override
   Widget build(BuildContext context) {
@@ -24,25 +22,35 @@ class PostView extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        _header(context),
-        _content(context),
-        _toolbar(context),
+        _PostHeader(post, header),
+        _PostContent(post),
+        _PostToolbar(post),
       ],
     );
   }
+}
 
-  Widget _header(BuildContext context) {
-    switch (header) {
-      case Header.simple:
-        return _simpleHeader(context);
-      case Header.extended:
-        return _extendedHeader(context);
+enum HeaderType { simple, extended }
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+  final HeaderType type;
+
+  _PostHeader(this.post, this.type);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (type) {
+      case HeaderType.simple:
+        return _simple(context);
+      case HeaderType.extended:
+        return _extended(context);
       default:
-        throw 'Header type [$header] not implemented';
+        throw 'Unrecognized type of header [$type]';
     }
   }
 
-  Widget _simpleHeader(BuildContext context) {
+  Widget _simple(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
       child: Column(
@@ -60,12 +68,13 @@ class PostView extends StatelessWidget {
     );
   }
 
-  Widget _extendedHeader(BuildContext context) {
+  Widget _extended(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return Container(
       padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           Row(
             children: <Widget>[
@@ -100,59 +109,9 @@ class PostView extends StatelessWidget {
             padding: EdgeInsets.only(top: 8),
             child: Text(
               post.title,
-              style: Theme.of(context).textTheme.title,
+              style: theme.textTheme.title,
             ),
           )
-        ],
-      ),
-    );
-  }
-
-  Widget _content(BuildContext context) {
-    switch (post.type) {
-      case ContentType.video:
-        return VideoContent(post);
-      case ContentType.gif:
-        return GifContent(post);
-      case ContentType.image:
-        return ImageContent(post);
-      case ContentType.text:
-        return TextContent(post);
-      default:
-        return LinkContent(post);
-    }
-  }
-
-  Widget _toolbar(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.arrow_drop_up, size: 32),
-            onPressed: null,
-          ),
-          Text(numberFormat.format(post.score)),
-          IconButton(
-            icon: Icon(Icons.arrow_drop_down, size: 32),
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(Icons.comment),
-            onPressed: null,
-          ),
-          Text(numberFormat.format(post.comments)),
-          Spacer(),
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: null,
-          ),
         ],
       ),
     );
@@ -205,6 +164,71 @@ class PostView extends StatelessWidget {
             ),
           ),
       ],
+    );
+  }
+}
+
+class _PostContent extends StatelessWidget {
+  final Post post;
+
+  _PostContent(this.post);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (post.type) {
+      case ContentType.video:
+        return VideoContent(post);
+      case ContentType.gif:
+        return GifContent(post);
+      case ContentType.image:
+        return ImageContent(post);
+      case ContentType.text:
+        return TextContent(post);
+      default:
+        return LinkContent(post);
+    }
+  }
+}
+
+class _PostToolbar extends StatelessWidget {
+  final Post post;
+  final NumberFormat numberFormat = NumberFormat.compact();
+
+  _PostToolbar(this.post);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_drop_up),
+            onPressed: null,
+          ),
+          Text(numberFormat.format(post.score)),
+          IconButton(
+            icon: Icon(Icons.arrow_drop_down),
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.comment),
+            onPressed: null,
+          ),
+          Text(numberFormat.format(post.comments)),
+          Spacer(),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () => Share.share(post.contentUrl),
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          ),
+        ],
+      ),
     );
   }
 }
