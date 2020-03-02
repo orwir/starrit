fdfed25159af5413f215cf7248601dce800df837
orwir <orwir.null@gmail.com>
2020-03-02 03:34:11 +0100

    small code improvements

--- lib/screens/feed/components/post.dart
+++ lib/screens/feed/components/post.dart
@@ -30,49 +30,6 @@ class PostView extends StatelessWidget {
   }
 }
 
-class _Toolbar extends StatelessWidget {
-  final Post post;
-  final NumberFormat numberFormat = NumberFormat.compact();
-
-  _Toolbar(this.post);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.arrow_drop_up),
-            onPressed: null,
-          ),
-          Text(numberFormat.format(post.score)),
-          IconButton(
-            icon: Icon(Icons.arrow_drop_down),
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(Icons.comment),
-            onPressed: null,
-          ),
-          Text(numberFormat.format(post.comments)),
-          Spacer(),
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () => Share.share(post.contentUrl),
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _Header extends StatelessWidget {
   final Post post;
   final HeaderType type;
@@ -228,6 +185,49 @@ class _Content extends StatelessWidget {
       default:
         return _LinkContent(post);
     }
+  }
+}
+
+class _Toolbar extends StatelessWidget {
+  final Post post;
+  final NumberFormat numberFormat = NumberFormat.compact();
+
+  _Toolbar(this.post);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_drop_up),
+            onPressed: null,
+          ),
+          Text(numberFormat.format(post.score)),
+          IconButton(
+            icon: Icon(Icons.arrow_drop_down),
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.comment),
+            onPressed: null,
+          ),
+          Text(numberFormat.format(post.comments)),
+          Spacer(),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () => Share.share(post.contentUrl),
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
   }
 }
 
