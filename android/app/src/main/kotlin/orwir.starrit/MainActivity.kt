d6741db4cf417ba651ecf1b062c45f040cc77c5c
orwir <orwir.null@gmail.com>
2020-05-17 02:39:57 +0200

    Android: added message channel for links

--- android/app/src/main/kotlin/orwir.starrit/MainActivity.kt
+++ android/app/src/main/kotlin/orwir.starrit/MainActivity.kt
@@ -1,12 +1,23 @@
 package orwir.starrit
 
-import androidx.annotation.NonNull;
+import android.content.Intent
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.StringCodec
 
 class MainActivity: FlutterActivity() {
+    lateinit var linksChannel: BasicMessageChannel<String>
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        linksChannel = BasicMessageChannel(flutterEngine.dartExecutor, "starrit/links", StringCodec.INSTANCE)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        linksChannel.send(intent.dataString)
     }
 }
